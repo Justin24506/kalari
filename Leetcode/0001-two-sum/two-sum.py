b29a4eab1656442e07945d732581fa552f282cfa
Justin <159104807+Justin24506@users.noreply.github.com>
2026-08-18 21:23:12 +0530

    sol(leetcode): 0001-two-sum optimal O(n) solution
    Problem: #0001 - Two Sum
    Language: Python 3
    Approach: Hash map lookup (Single Pass)
    
    Stats:
      - Runtime: 0 ms (O(n))
      - Memory:  20.34 MB (O(n))
    
    Changes:
      - Replaced O(n^2) brute force scan with dictionary storage
      - Stored target complements on-the-fly to prevent self-matching

--- Leetcode/0001-two-sum/two-sum.py
+++ Leetcode/0001-two-sum/two-sum.py
@@ -1,12 +1,15 @@
-# https://leetcode.com/problems/two-sum/submissions/2111531306
-# Runtime 1734 ms
-# Memory 19.83 MB
+# https://leetcode.com/problems/two-sum/submissions/2111626253
+# Runtime 0 ms
+# Memory 20.34 MB
 
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        dict_nums = {}
         for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return [i, j]
+            rem = target - nums[i]
+            if rem not in dict_nums:
+                dict_nums[nums[i]] = i
+            else:
+                return [i, dict_nums[rem]]
         return []
