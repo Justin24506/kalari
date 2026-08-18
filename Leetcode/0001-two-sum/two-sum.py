99aba6580bd47d691e407856810d3adb5572e7ce
Justin <159104807+Justin24506@users.noreply.github.com>
2026-08-18 20:00:09 +0530

    sol(leetcode): optimize 0001-two-sum with early return
    [Problem]     #0001 - Two Sum
    [Language]    Python 3
    [Approach]    Brute Force (Early Return)
    [Time]        O(n^2)
    [Space]       O(1)
    [Notes]       Added early return on match and fallback return for type
    safety

--- Leetcode/0001-two-sum/two-sum.py
+++ Leetcode/0001-two-sum/two-sum.py
@@ -1,14 +1,12 @@
-# https://leetcode.com/problems/two-sum/submissions/2111502116
-# Runtime 2083 ms
-# Memory 19.72 MB
+# https://leetcode.com/problems/two-sum/submissions/2111531306
+# Runtime 1734 ms
+# Memory 19.83 MB
 
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        output = []
         for i in range(0, len(nums)):
             for j in range(i + 1, len(nums)):
-                sum = nums[i] + nums[j]
-                if sum == target:
-                    output = [i, j]
-        return output
+                if (nums[i] + nums[j]) == target:
+                    return [i, j]
+        return []
